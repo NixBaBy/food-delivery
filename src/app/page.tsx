0ec83f35e13258de9ae522592ad67c513bc84861
NixBaBy <115783986+NixBaBy@users.noreply.github.com>
2025-02-19 23:38:27 +0800

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
+import Image from "next/image";
 import CloudinaryUpload from "./(admin)/admin/components/CloudinaryUpload";
 import Authlayout from "./(main)/layout";
+import Categorys from "@/components/Categorys";
 
 export default function Home() {
   return (
-    <Authlayout>
-      <div></div>
-    </Authlayout>
+    <div className="bg-[#404040]">
+      <Authlayout>
+        <div
+          style={{
+            backgroundImage: `url("https://res.cloudinary.com/dhvtog3m2/image/upload/v1739972500/a0g2m0njixoflsowgo8f.png")`,
+          }}
+          className="w-full h-[570px] bg-cover bg-center"
+        ></div>
+        <Categorys />
+      </Authlayout>
+    </div>
   );
 }
