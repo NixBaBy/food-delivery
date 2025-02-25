eb35bcab336bbb9c24bdb24c51f316e7e8b7c336
tuguldur <115783986+NixBaBy@users.noreply.github.com>
2025-02-25 18:06:05 +0800

    changes

--- src/app/(main)/components/Categorys.tsx
+++ src/app/(main)/components/Categorys.tsx
@@ -1,12 +1,23 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Foods from "./Foods";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
 type Category = {
   id: number;
   categoryName: string;
 };
+
 const Categorys = () => {
   const [categories, setCategories] = useState<Category[]>([]);
+
   useEffect(() => {
     const getData = async () => {
       const res = await fetch(`http://localhost:8080/api/categories`);
@@ -15,24 +26,31 @@ const Categorys = () => {
     };
     getData();
   }, []);
+
   return (
     <div>
       <div className="flex flex-col gap-9 px-[48px] py-8">
         <h2 className="text-white font-bold tracking-[-2.5%] text-[30px]">
           Categories
         </h2>
-        <div className="flex gap-[8px]">
-          {categories.map((category) => {
-            return (
-              <button
+        <Carousel>
+          <CarouselContent className="flex gap-2">
+            {categories.map((category) => (
+              <CarouselItem
                 key={category.id}
-                className=" py-1 px-5 bg-white rounded-full"
+                className="flex-shrink-0 w-[200px]"
               >
-                {category.categoryName}
-              </button>
-            );
-          })}
-        </div>
+                {" "}
+                {/* Тохируулсан хэмжээ */}
+                <button className="py-1 px-5 bg-white rounded-full">
+                  {category.categoryName}
+                </button>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious className="absolute top-1/2 left-0 z-10" />
+          <CarouselNext className="absolute top-1/2 right-0 z-10" />
+        </Carousel>
       </div>
       <Foods />
     </div>
--- src/app/(main)/components/Foods.tsx
+++ src/app/(main)/components/Foods.tsx
@@ -6,13 +6,29 @@ type Food = {
   category: number;
   id: number;
 };
-const Foods = async () => {
-  const res = await fetch(`http://localhost:8080/api/foods`);
-  const data = await res.json();
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useEffect, useState } from "react";
+const Foods = () => {
+  const [foods, setFoods] = useState<Food[]>([]);
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch(`http://localhost:8080/api/foods`);
+      const data = await res.json();
+      setFoods(data);
+    };
+    getData();
+  }, []);
 
   return (
     <div className="flex gap-5 flex-wrap w-[1264px] m-auto">
-      {data?.map((food: Food) => {
+      {foods?.map((food: Food) => {
         return (
           <div
             key={food.id}
@@ -20,8 +36,38 @@ const Foods = async () => {
           >
             <div
               style={{ backgroundImage: `url(${food.image})` }}
-              className="bg-cover bg-center w-[365px] h-[210px] rounded-xl "
-            ></div>
+              className="bg-cover bg-center w-[365px] h-[210px] rounded-xl relative"
+            >
+              <Dialog>
+                <DialogTrigger>
+                  <div className="w-[44px] h-[44px] py-2 px-4 justify-center items-center text-red-500 rounded-full bg-[#FFF] absolute bottom-5 right-5">
+                    +
+                  </div>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle></DialogTitle>
+                    <DialogDescription
+                      ref={null}
+                      className="text-sm text-muted-foreground w-826px h-[412px] p-6 flex gap-6 rounded-[20px]"
+                    >
+                      <div className="flex gap-6">
+                        <img
+                          src={food.image}
+                          className="w-full h-full rounded-xl"
+                        />
+                        <div className="flex flex-col gap-3">
+                          <p className="text-red-500 text-[24px] font-bold tracking-[-0.6px]">
+                            {food.foodName}
+                          </p>
+                          <p className="text-[14px]">{food.ingredients}</p>
+                        </div>
+                      </div>
+                    </DialogDescription>
+                  </DialogHeader>
+                </DialogContent>
+              </Dialog>
+            </div>
             <div className="flex flex-col gap-2">
               <div className="flex items-center justify-between">
                 <p className="text-red-500 text-[24px] font-bold tracking-[-0.6px] ">
